101e2a04f293ec40f79554f41ea224c9c36df4f6
Giovanni Marques <giovanni.marques030@gmail.com>
2024-10-18 12:08:27 -0300

    chore: add template project

---
+++ src/main/java/com/talkspace/TalkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.talkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkspaceApplication.class, args);
+	}
+
+}
